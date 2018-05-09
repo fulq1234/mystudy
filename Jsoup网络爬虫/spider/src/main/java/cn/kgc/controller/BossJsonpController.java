7d3e396ff4f251d04a84d8fd0f612e2f31c5608b
fulq1234 <543163041@qq.com>
2018-05-09 09:41:45 +0800

    去掉没用的

--- Jsoup网络爬虫/spider/src/main/java/cn/kgc/controller/BossJsonpController.java
+++ Jsoup网络爬虫/spider/src/main/java/cn/kgc/controller/BossJsonpController.java
@@ -32,7 +32,8 @@ public class BossJsonpController {
 		String url = "https://www.zhipin.com/c101010100/h_101010100/?query=java&page=temp&ka=page-temp";
 		
 		ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);//创建一个定长线程池，可控制线程最大并发数，超过的线程会在队列中等待。
-		for(int pageii = 0;pageii<50;pageii++){//爬几页
+		//for(int pageii = 0;pageii<50;pageii++){//爬几页
+		for(int pageii = 0;pageii<1;pageii++){//爬一页
 			final String listUrl = url.replaceAll("temp", (pageii+1) + "");
 			fixedThreadPool.execute(new Runnable(){
 
@@ -44,65 +45,65 @@ public class BossJsonpController {
 						Document document = DocumentUtilFree.getDocument(listUrl);//Jsoup.connect(listUrl).get();
 						String selector = "h3[class=name]>a";
 						Elements elements = document.select(selector);
-						/*for(int i = 0;i<elements.size();i++){
+						for(int i = 0;i<elements.size();i++){
+							Element e = elements.get(0);//职位标题
+							String title = e.text();
 							
-						}*/
-						Element e = elements.get(0);//职位标题
-						String title = e.text();
-						
-						String detailstr = e.absUrl("href");
-						
-						Document detailD = DocumentUtilFree.getDocument(detailstr);
-						
-						//城市：石家庄经验：5-10年学历：本科
-						String threestr = detailD.select("div .info-primary").select("p").html();
-						String[] arr = threestr.split("<em class=\"vline\"></em>");
-						String companyAddress = "";
-						String experience = "";
-						if(arr.length >=2){
-							companyAddress = arr[0];//公司地址
-							experience =arr[1];//工作经验
+							String detailstr = e.absUrl("href");
 							
+							Document detailD = DocumentUtilFree.getDocument(detailstr);
+							
+							//城市：石家庄经验：5-10年学历：本科
+							String threestr = detailD.select("div .info-primary").select("p").html();
+							String[] arr = threestr.split("<em class=\"vline\"></em>");
+							String companyAddress = "";
+							String experience = "";
+							if(arr.length >=2){
+								companyAddress = arr[0];//公司地址
+								experience =arr[1];//工作经验
+								
+							}
+							
+							//公司名称
+							String companyName = detailD.select("div .info-company").select(".name").text();
+							
+							//公司性质
+							String companyss = detailD.select("div .info-company").select("p").html();
+							String[] carr = companyss.split("<em class=\"vline\"></em>");
+							String companyType = carr[0];
+							
+							//工作描述
+							String jobDescription = detailD.select(".job-sec").select(".text").text();
+							
+							System.out.println(jobDescription);
+							
+							
+							//插入数据
+							Recruit recruit = new Recruit();
+							recruit.setTitle(title);//标题
+							recruit.setDetailUrl(detailstr);//招聘详情页
+							
+							//公司地址
+							recruit.setCompanyAddress(companyAddress);
+							//工作经验
+							recruit.setExperience(experience);
+							//公司名称
+							recruit.setCompanyName(companyName);
+							//工作详情
+							recruit.setJobDescription(jobDescription);
+							
+							recruit.setTaskId(1);
+							recruit.setDataType(1);
+							recruit.setStatus(0);
+							recruit.setCompanyType(companyType);//不能为空
+							try {
+								recruitService.itriptxAddRecruit(recruit);
+							} catch (Exception e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
 						}
 						
-						//公司名称
-						String companyName = detailD.select("div .info-company").select(".name").text();
-						
-						//公司性质
-						String companyss = detailD.select("div .info-company").select("p").html();
-						String[] carr = companyss.split("<em class=\"vline\"></em>");
-						String companyType = carr[0];
-						
-						//工作描述
-						String jobDescription = detailD.select(".job-sec").select(".text").text();
-						
-						System.out.println(jobDescription);
-						
-						
-						//插入数据
-						Recruit recruit = new Recruit();
-						recruit.setTitle(title);//标题
-						recruit.setDetailUrl(detailstr);//招聘详情页
-						
-						//公司地址
-						recruit.setCompanyAddress(companyAddress);
-						//工作经验
-						recruit.setExperience(experience);
-						//公司名称
-						recruit.setCompanyName(companyName);
-						//工作详情
-						recruit.setJobDescription(jobDescription);
-						
-						recruit.setTaskId(1);
-						recruit.setDataType(1);
-						recruit.setStatus(0);
-						recruit.setCompanyType(companyType);//不能为空
-						try {
-							recruitService.itriptxAddRecruit(recruit);
-						} catch (Exception e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
 						
 						
 					} catch (Exception e) {
